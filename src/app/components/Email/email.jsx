fd53e794b75c233295910998ad28bb570229fa18
Luiforr <rizkyrofiul@gmail.com>
2024-04-14 21:10:00 +0700

    contact ui

--- src/app/components/Email/email.jsx
+++ src/app/components/Email/email.jsx
@@ -18,73 +18,88 @@ import {
   Image,
   Spacer,
   Wrap,
-  WrapItem
+  WrapItem,
 } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { RiSearchLine } from "react-icons/ri";
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBackward,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBackward } from "@fortawesome/free-solid-svg-icons";
 export default function Email() {
   return (
     <div className="mb-96">
       <div className="flex-1 p-4">
-        <div className="mb-4 text-center">
-          <h1 className="text-2xl font-semibold mb-2 text-center">
+        <div className="mb-1 text-center">
+          <h1 className="text-2xl font-semibold mb-2 text-center text-blue-500">
             Kontak masuk
           </h1>
-          <div className="mb-1">
-            <Stack spacing={3}>
-              <Input variant="filled" placeholder="Cari Kontak..." />
-            </Stack>
-          </div>
         </div>
       </div>
-      <Card className="w-full">
-        <CardHeader className="border-1 border-gray-200">
-          <Flex spacing="4">
-            <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-              <div className="justify-start items-start">
-                <div className="avatar placeholder ">
-                  <div className="bg-purple-600 text-white rounded-full w-12 ">
-                    <span class="text-lg">U</span>
+      <div className="">
+        <Card className="w-full">
+          <CardHeader className="border-1 border-gray-200 bg-gray-100">
+            <Flex spacing="4">
+              <Flex flex="1" gap="2" alignItems="start" flexWrap="wrap">
+                <div className="justify-start items-start">
+                  <div className="avatar placeholder ">
+                    <div className="bg-blue-600 text-white rounded-full w-12">
+                      <span class="text-lg">U</span>
+                    </div>
                   </div>
                 </div>
+                <Box>
+                <p className="font-bold sm: text-sm md:text-sm lg:text-md xl:text-md ">
+                    Rizky rofiul mutaqin
+                  </p>
+                  <p className=" sm: text-xs md:text-xs lg:text-sm xl:text-sm ">
+                    wande@gmail.com
+                  </p>
+                </Box>
+              </Flex>
+              <div className="justify-end">
+                <p className=" sm: text-xs md:text-xs lg:text-sm xl:text-sm ">
+                  14/4/2024
+                </p>
               </div>
-              <Box>
-                <Heading size="sm" justify="start" items="start">
-                  wawan Adebayo
-                </Heading>
-                <Text fontSize="xs">2003-30-22-20:39</Text>
-              </Box>
-
-            
             </Flex>
-          </Flex>
-        </CardHeader>
-        <CardBody>
-              <Box>
-                <Text>
-                  masukan keluhan anda Lorem ipsum dolor sit, amet consectetur
-                     </Text>
-                <Text pt="2" fontSize="sm">
-                  <div className="">
-                    <p>
-                      <Image
-                        className="float-right"
-                        width={400}
-                        height={250}
-                        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-                        alt=""
-                      />
-                    </p>
+          </CardHeader>
+          <CardBody className="bg-gray-50">
+            <p className="sm: text-sm md:text-sm lg:text-sm xl:text-sm">
+              masukan keluhan anda Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Eaque libero minima ea. Optio assumenda,
+              voluptatibus eum deserunt mollitia quaerat dolore neque cumque
+              sunt. Iusto, quia quam? Similique pariatur doloremque ipsam
+              suscipit dolorum deleniti officiis numquam rem facilis, aperiam
+              aliquid repudiandae. Doloribus, error culpa eum maxime repellat
+              excepturi nostrum quo iusto!
+            </p>
+          </CardBody>
+        </Card>
+        {/* <Card className="w-full">
+          <CardHeader className="border-1 border-gray-200 bg-gray-100 cursor-pointer">
+            <Flex spacing="4">
+              <Flex flex="1" gap="4" alignItems="start" flexWrap="wrap">
+                <div className="justify-start items-start">
+                  <div className="avatar placeholder ">
+                    <div className="bg-blue-600 text-white rounded-full w-12 ">
+                      <span class="text-lg">U</span>
+                    </div>
                   </div>
-                </Text>
-              </Box>
-        </CardBody>
-      </Card>
+                </div>
+                <Box>
+                  <Heading size="sm" justify="start" items="start">
+                    wawan Adebayo
+                  </Heading>
+                  <Text fontSize="sm">anggap aja judul </Text>
+                </Box>
+              </Flex>
+              <Text fontSize="xs" justify="end" items="end">
+                14/4/2024
+              </Text>
+            </Flex>
+          </CardHeader>
+        </Card> */}
+      </div>
     </div>
   );
 }
