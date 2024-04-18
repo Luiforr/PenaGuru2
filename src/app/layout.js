ebcb7a83b06044c2617049625344c343ad626ffa
Ri's Dev Ri阿里爱娜 <142820574+Ririena@users.noreply.github.com>
2024-04-18 21:21:37 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,47 +8,46 @@ const inter = Inter({ subsets: ["latin"] });
 import BottomNavigation from "./components/Mobile/BotNav";
 import AuthUser from "./middleware/user"
 export const metadata = {
- metadataBase: new URL('https://penaguru-re.vercel.app/'),
-3  title: {
-4    default: 'https://penaguru-re.vercel.app/',
-5    template: `$%s - https://penaguru-re.vercel.app/`,
-6  },
-7  description: 'Sebuah Website Presensi SMKN7 Untuk Guru Supaya Pendataan Lebih Mudah Dan Praktis',
-8  keywords: [
-9    'SMKN7 Baleendah', 'SMKN7 BE', 'SMKN7 PenaGuru', 'PenaGuru SMKN7 Baleendah', 'PenaGuru', 'PenaGuruRE', 'PEGU SMKN7', 'Presensi SMKN7 Baleendah', 'Absensi Guru SMKN7 Baleendah', 'Pena Guru', 'Guru Pena'
-10  ],
-11  authors: [
-12    
-13      {
-14        name: "Rizki Rofiul Muttakin",
-15        url: "https://penaguru-re.vercel.app/",
-16      }
-17    
-18  ],
-19  creator: 'Adlin Qaidul Jaisy, Nabhan Alzam Faturrohman, Muhammad Aqil Fatahillah',
-20  themeColor: [
-21    { media: "(prefers-color-scheme: light)", color: "white" },
-22    { media: "(prefers-color-scheme: dark)", color: "black" },
-23  ],
-24  openGraph: {
-25    type: "website",
-26    locale: "en_US",
-27    url: 'https://penaguru-re.vercel.app/',
-28    title: 'https://penaguru-re.vercel.app/',
-29    description: 'Sebuah Website Presensi SMKN7 Untuk Guru Supaya Pendataan Lebih Mudah Dan Praktis',
-30    siteName: 'https://penaguru-re.vercel.app/',
-31  },
-32  twitter: {
-33    card: "summary_large_image",
-34    title: 'https://penaguru-re.vercel.app/',
-35    description: 'Sebuah Website Presensi SMKN7 Untuk Guru Supaya Pendataan Lebih Mudah Dan Praktis',
-36    images: '${siteUrl}/og.jpg',
-37    creator: '@@nothing',
-38  },
-39  icons: {
-40    icon: "/favicon.ico",
-41  },
+  metadataBase: new URL('https://penaguru-re.vercel.app/'),
+  title: {
+    default: 'https://penaguru-re.vercel.app/',
+    template: `%s - https://penaguru-re.vercel.app/`,
+  },
+  description: 'Sebuah Website Presensi SMKN7 Untuk Guru Supaya Pendataan Lebih Mudah Dan Praktis',
+  keywords: [
+    'SMKN7 Baleendah', 'SMKN7 BE', 'SMKN7 PenaGuru', 'PenaGuru SMKN7 Baleendah', 'PenaGuru', 'PenaGuruRE', 'PEGU SMKN7', 'Presensi SMKN7 Baleendah', 'Absensi Guru SMKN7 Baleendah', 'Pena Guru', 'Guru Pena'
+  ],
+  authors: [
+    {
+      name: "Rizki Rofiul Muttakin",
+      url: "https://penaguru-re.vercel.app/",
+    }
+  ],
+  creator: 'Adlin Qaidul Jaisy, Nabhan Alzam Faturrohman, Muhammad Aqil Fatahillah',
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: 'https://penaguru-re.vercel.app/',
+    title: 'PenaGuru - https://penaguru-re.vercel.app/',
+    description: 'Sebuah Website Presensi SMKN7 Untuk Guru Supaya Pendataan Lebih Mudah Dan Praktis',
+    siteName: 'https://penaguru-re.vercel.app/',
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: 'PenaGuru - https://penaguru-re.vercel.app/',
+    description: 'Sebuah Website Presensi SMKN7 Untuk Guru Supaya Pendataan Lebih Mudah Dan Praktis',
+    images: '${siteUrl}/og.jpg',
+    creator: '@@nothing',
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
