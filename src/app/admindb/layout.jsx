2d5520e2000be6afb6e28bb19e234e447874597a
Ririena <nerllysurina@gmail.com>
2024-04-19 20:29:39 +0700

    Add AdminLayout

---
+++ src/app/admindb/layout.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Divider } from "@nextui-org/react";
+export default function AdminDashboard({ children }) {
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row h-screen">
+        <aside className="lg:w-64 bg-gray-800">
+          <div className="p-4 text-white">
+            <h1 className="text-lg font-bold">Admin Panel</h1>
+            <Divider className="bg-gray-300 mt-2" />
+            <ul className="mt-4">
+              <li className="py-2">
+                <Link href="#" className="text-gray-300 hover:text-white">
+                  Dashboard
+                </Link>
+              </li>
+              <li className="py-2">
+                <Link href="#" className="text-gray-300 hover:text-white">
+                  Users
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </aside>
+        <main className="flex-1 bg-gray-200">
+          <nav className="bg-white shadow p-4">
+            <div className="container mx-auto">
+              <h1 className="text-xl font-bold">Welcome, Ariena!</h1>
+            </div>
+          </nav>
+          <div className="container mx-auto p-4">
+            <section>{children}</section>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
